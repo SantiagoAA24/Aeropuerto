ae796c1d8d6fb90e83dab04c6fd9ff8e3c0a1f1b
Santiago Acevedo <santiago.acevedo.m24@gmail.com>
2022-08-25 19:20:44 -0500

    Inicio del proyecto APi Aeropuerto

--- Aeropuerto_app/views.py
+++ Aeropuerto_app/views.py
@@ -6,3 +6,7 @@ from rest_framework import viewsets, status
 class Avion_view(viewsets.ModelViewSet):
     queryset = Avion.objects.all().order_by('codigo_avion')
     serializer_class = Avion_Serializer
+
+class Piloto_view(viewsets.ModelViewSet):
+    queryset = Piloto.objects.all().order_by('codigo_piloto')
+    serializer_class = Piloto_Serializer
