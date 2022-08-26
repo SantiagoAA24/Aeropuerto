9a2e64f5cf36e31fdf4d6df89e7e371ff101f991
Santiago Acevedo <santiago.acevedo.m24@gmail.com>
2022-08-25 19:17:42 -0500

    Inicio del proyecto APi Aeropuerto

---
+++ Aeropuerto_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from Aeropuerto_app.models import *
+from Aeropuerto_app.serializers import *
+from rest_framework import viewsets, status
+
+class Avion_view(viewsets.ModelViewSet):
+    queryset = Avion.objects.all().order_by('codigo_avion')
+    serializer_class = Avion_Serializer
